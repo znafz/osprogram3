1974f9d04569ef7762b2726a97d34517dfdefde2
Zach Nafziger <zachary@nafziger.net>
2015-04-27 22:02:42 -0400

    task 2 improvements
    to make it have the same output format as task1

--- task2.c
+++ task2.c
@@ -21,58 +21,110 @@ int main(int argc, char** argv) {
        exit(1);         /* Program exits if file pointer returns NULL. */
    }
 
-   //read out the free memory
-	printf("\nFree Memory Before:\n\t");
+   int total_mem, free_mem, used_mem;
+	char * temp;
+	char * token;
+	printf("Memory Before:\n");
+   fgets(c,100,ifp);//total memory
+   temp = strdup(c);
+   int i;
+   for(i=0; i < 9; i++)
+		token = strsep(&temp, "  "); // had to repeat strsep for some reason
+   total_mem = atoi(token);
+   printf("\tTotal Memory: %d\n", total_mem);
+
+
+   //now get free memory
    fgets(c,100,ifp);
-   fgets(c,100,ifp);
-   printf("%s",c);
+   temp = strdup(c); 
+   for(i=0; i < 11; i++)
+		token = strsep(&temp, "  "); // had to repeat strsep for some reason
+   free_mem = atoi(token);
+   printf("\tFree Memory: %d\n", free_mem);
+
+   //calculate memory in use
+   used_mem = total_mem - free_mem;
+   printf("\tUsed Memory: %d\n", used_mem);
+
 
    //start some processes
    
    if(fork() == 0){
-	   
+	   printf("executing process 1..\n");
 	   execv("ping", "ping google.com");
 	   
    } else{
-
+	   
 	   if(fork() == 0){
-		   sleep(1.9);
+		   usleep(1000000);//1 million microseconds =  1 second
+		   printf("executing process 2..\n");
 		   execv("ping", "ping youtube.com");
 	   
 	   } else{
-
+		   
 			   if(fork() == 0){
-			   sleep(2.9);
-			   execv("ping", "ping facebook.com");
+					usleep(2000000);
+					printf("executing process 3..\n");
+					execv("ping", "ping facebook.com");
 	   
 		   } else{
 
 			   //check the first process
-			   sleep(1);//sleep a little so it doesn't check memory until after the process has been running a bit
+			   usleep(500000);//sleep a little so it doesn't check memory until after the process has been running a bit
 			   //rewind to the beginning of meminfo and read again
-			   printf("After Process 1 starts:\n\t");
+			   printf("After Process 1 starts:\n");
 			   rewind(ifp);
 			   fgets(c,100,ifp);
 			   fgets(c,100,ifp);
-			   printf("%s",c);
+			   temp = strdup(c); 
+			   for(i=0; i < 11; i++)
+					token = strsep(&temp, "  "); // had to repeat strsep for some reason
+			   free_mem = atoi(token);
+			   printf("\tFree Memory: %d\n", free_mem);
+
+			   //calculate memory in use
+			   int old_used_mem = used_mem;
+			   used_mem = total_mem - free_mem;
+			   printf("\tUsed Memory: %d\n", used_mem);
+			   printf("\tMemory Used By Process: %d\n", used_mem-old_used_mem);
 
 			   //check the second process
-			    sleep(1);//sleep a little so it doesn't check memory until after the process has been running a bit
+			    usleep(1000000);//sleep a little so it doesn't check memory until after the process has been running a bit
 			   //rewind to the beginning of meminfo and read again
-			   printf("After Process 2 starts:\n\t");
+			   printf("After Process 2 starts:\n");
 			   rewind(ifp);
 			   fgets(c,100,ifp);
 			   fgets(c,100,ifp);
-			   printf("%s",c);
+			   temp = strdup(c); 
+			   for(i=0; i < 11; i++)
+					token = strsep(&temp, "  "); // had to repeat strsep for some reason
+			   free_mem = atoi(token);
+			   printf("\tFree Memory: %d\n", free_mem);
+
+			   //calculate memory in use
+			   old_used_mem = used_mem;
+			   used_mem = total_mem - free_mem;
+			   printf("\tUsed Memory: %d\n", used_mem);
+			   printf("\tMemory Used By Process: %d\n", used_mem-old_used_mem);
 
 			   //check the third process
-			   sleep(1);//sleep a little so it doesn't check memory until after the process has been running a bit
+			   usleep(1000000);//sleep a little so it doesn't check memory until after the process has been running a bit
 			   //rewind to the beginning of meminfo and read again
-			   printf("After Process 3 starts:\n\t");
+			   printf("After Process 3 starts:\n");
 			   rewind(ifp);
 			   fgets(c,100,ifp);
 			   fgets(c,100,ifp);
-			   printf("%s",c);
+			   temp = strdup(c); 
+			   for(i=0; i < 11; i++)
+					token = strsep(&temp, "  "); // had to repeat strsep for some reason
+			   free_mem = atoi(token);
+			   printf("\tFree Memory: %d\n", free_mem);
+
+			   //calculate memory in use
+			   old_used_mem = used_mem;
+			   used_mem = total_mem - free_mem;
+			   printf("\tUsed Memory: %d\n", used_mem);
+			   printf("\tMemory Used By Process: %d\n", used_mem-old_used_mem);
 
 		  }
 	   }
