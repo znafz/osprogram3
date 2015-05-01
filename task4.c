7c80f95a4f96b69daee5ab0756f91e3973d9df12
Aaron <aaronr93@gmail.com>
2015-05-01 12:18:54 -0400

    Changes

--- task4.c
+++ task4.c
@@ -2,7 +2,7 @@
 Author:  Zach Nafziger and Aaron Rosenberger
 Course:  COMP 340, Operating Systems
 Date:    10 March 2015
-Description: Ends user processes when memory allocation exceeds given limit.
+Description:
 Compile with:  gcc -o task4 task4.c
 Run with:  ./task4
 
@@ -12,6 +12,7 @@ Run with:  ./task4
 
 #include <stdio.h>
 #include <stdlib.h> /* For exit() function*/
+#include <time.h>
 
 double get_file_value(FILE * ifp, int target) {
 	int value = 0.0;
@@ -36,6 +37,9 @@ double get_file_value(FILE * ifp, int target) {
 
 int main(int argc, char** argv) {
 
+	clock_t start = clock();
+	int current_time;
+
 	//first open meminfo
 	FILE *ifp = fopen("/proc/meminfo", "r");
 
@@ -45,29 +49,42 @@ int main(int argc, char** argv) {
 	}
 
 	double total_mem, free_mem, used_mem, percentage;
-	int flag = 10;
 
-	while (flag) {
-		total_mem = get_file_value(ifp, 0);	//total memory
-		free_mem = get_file_value(ifp, 1);	//now get free memory
+	system("clear");
+
+	while (1) {
+		total_mem = get_file_value(ifp, 0);	// /proc/meminfo:MemTotal
+		free_mem = get_file_value(ifp, 1);	// /proc/meminfo:MemFree
 		used_mem = total_mem - free_mem;	//calculate memory in use
 
+		printf("\033[7A"); // Move up 7 lines
+		printf("\033[10D"); // Move left 10 columns
 		printf("\n\tTotal:\t%.0f\n\tFree:\t%.0f\n\tUsed:\t%.0f\n", total_mem, free_mem, used_mem);
+		printf("\tTime: %d\n", clock() / CLOCKS_PER_SEC);
 
 		percentage = (used_mem / total_mem) * 100;
 
 		printf("\t----- Used: %.0f%% -----\n", percentage);
-		if (used_mem / total_mem > LIMIT) {
+		if (1/*used_mem / total_mem > LIMIT*/) {
 			// Memory limit exceeded
 			printf("Memory limit of %.0f%% reached.\n", LIMIT * 100);
 
-			// Now kill user processes... TODO!
+			// Now kill user processes...
+			if (fork() == 0) {
+				//system("gcc -o memhog memhog.c -lm");
+				//system("./memhog");
+				//system("pkill ping");
+				//exit(1);
+			}
 		}
 
-		sleep(1);
-		flag--;
+		if (clock() / CLOCKS_PER_SEC >= 15) exit(0);
+
 	}
 
 	fclose(ifp);
 	return 0;
 }
+
+
+
