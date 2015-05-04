f5f261bbb1cd4499f859da230f00e7b04ec3758c
Aaron <aaronr93@gmail.com>
2015-05-04 13:06:45 -0400

    Replaced freemem with task3

--- task3.c
+++ task3.c
@@ -1,32 +1,26 @@
 /*
-Author:  Zach Nafziger and Aaron Rosenberger
-Course:  COMP 340, Operating Systems
-Date:    10 March 2015
-Description: Task 3
-Compile with:  gcc -o task3 task3.c
-Run with:  ./task3
+  Author:  Zach Nafziger and Aaron Rosenberger
+  Course:  COMP 340, Operating Systems
+  Date:    10 March 2015
+  Description:
+  Compile with:  gcc -o freemem freemem.c
+  Run with:  ./freemem
 
-*/
+  */
 
-#include <sys/types.h>
-#include <sys/stat.h>
+#define LIMIT 0.85
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include <syslog.h>
-#include <string.h>
+#include <stdlib.h> /* For exit() function*/
 #include <time.h>
-#include <math.h>
 
-int get_file_value(FILE * ifp, int target) {
-	int value = 0;
+double get_file_value(FILE * ifp, int target) {
+	int value = 0.0;
 	char name[15];
 	char trash[100];
 
 	/* If target == 1, looking for MemFree
-	   If target == 0, looking for MemTotal */
+	If target == 0, looking for MemTotal */
 
 	fscanf(ifp, "%s %d", name, &value);		// Get number & "Kb"
 	fgets(trash, 50, ifp);
@@ -38,11 +32,14 @@ int get_file_value(FILE * ifp, int target) {
 
 	rewind(ifp);
 
-	return value;
+	return (double)value;
 }
 
 int main(int argc, char** argv) {
 
+	clock_t start = clock();
+
+	//first open meminfo
 	FILE *ifp = fopen("/proc/meminfo", "r");
 
 	if (ifp == NULL) {
@@ -50,47 +47,33 @@ int main(int argc, char** argv) {
 		exit(1);         /* Program exits if file pointer returns NULL. */
 	}
 
-	int i;
-	int total_mem, free_mem, used_mem;
-
-	total_mem = get_file_value(ifp, 0);		// Total Memory
-	free_mem = get_file_value(ifp, 1);		// Free Memory
-
-	//calculate memory in use
-	used_mem = total_mem - free_mem;
-	printf("\tUsed Memory: %d\n", used_mem);
-
-	int flag = 5;
-	int old_used_mem = used_mem;
+	double total_mem, free_mem, used_mem, percentage;
 	
-	if (fork() == 0) {
-		while (flag--) {
-			free_mem = get_file_value(ifp, 1);
+	while (1) {
+		total_mem = get_file_value(ifp, 0);	// /proc/meminfo:MemTotal
+		free_mem = get_file_value(ifp, 1);	// /proc/meminfo:MemFree
+		used_mem = total_mem - free_mem;	//calculate memory in use
+		
+		//printf("\033[7A"); // Move up 7 lines
+		//printf("\033[10D"); // Move left 10 columns
+		system("clear");
+		printf("\n\tTotal:\t%.0f\tFree:\t%.0f\tUsed:\t%.0f", total_mem, free_mem, used_mem);
+		printf("\tTime: %d", clock() / CLOCKS_PER_SEC);
 
-			used_mem = total_mem - free_mem;
-			printf("\t\tUsed Memory: %d\n", used_mem);
-			printf("\t\tMemory Used By Process: %d\n", used_mem - old_used_mem);
+		percentage = (used_mem / total_mem) * 100;
+
+		printf("\n\t----- Used: %.0f%% -----\n", percentage);
+		if (used_mem / total_mem > LIMIT) {
+			// Memory limit exceeded
+			printf("Memory limit of %.0f%% reached.\n", LIMIT * 100);
 		}
-		fclose(ifp);
-		sleep(5);
-	} else {
-		printf("\nPARENT\n\n");
-		flag = 50;
-		int j, k, ugh;
-		ugh = 1;
-		while (flag--) {
-			for (i = 0; i < 100; i++) {
-				for (k = 1; k <= 100; k++) {
-					for (j = 0; j < 100; j++) {
-						ugh += 1;
-					}
-				}
-			}
-		}
-		fclose(ifp);
+		
+		if (clock() / CLOCKS_PER_SEC >= 15) exit(0);
+
+		sleep(1);
+
 	}
 
-	printf("\n\nugh\n\n");
-
+	fclose(ifp);
 	return 0;
 }
